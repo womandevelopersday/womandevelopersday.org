a640db6d6497345c1249c9192f336be8e2c4c6c8
KEMAL KANOK <kemalkanok@gmail.com>
2014-05-09 15:55:08 +0300

    fix #64 , clock as well

--- js/clock.js
+++ js/clock.js
@@ -16,9 +16,20 @@ function calctime()
 	saat = result % 24;
 	result = divInt(result,24);
 	gun = result;
+	if(result < 0)
+	{
+		gun = 0;
+		dk = 0;
+		saat = 0;
+		$("#timeleft").html("<span>"+ gun + "</span><span>Gün</span><span>" + saat + "</span><span>Saat</span><span> " + dk + "</span> <span>Dakika</span>");
 
-	$("#timeleft").html("<span>"+ gun + "</span><span>Gün</span><span>" + saat + "</span><span>Saat</span><span> " + dk + "</span> <span>Dakika</span>");
+	}
+	else
+	{
+		$("#timeleft").html("<span>"+ gun + "</span><span>Gün</span><span>" + saat + "</span><span>Saat</span><span> " + dk + "</span> <span>Dakika</span>");
 	setTimeout(function() {
 		calctime();
 	}, 999);
+	}
+	
 }
